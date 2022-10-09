6266ea8a81032baf30d8ebe5053d65f728726ece
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-09 12:32:16 +0300

    assets

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Vadim Savin",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.post}>
+        {/* Header */}
+        <View style={styles.header}>
+          <Image source={{uri: post.image}} />
+
+        </View>
+
+        {/* Body */}
+
+        {/* Footer */}
+        <View style={styles.footer}>
+
+        </View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
